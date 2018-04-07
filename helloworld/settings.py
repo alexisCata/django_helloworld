0775e974fbf270182f27ce488ba288761f4f3cf1
alexis <a.flores@cathedralsw.com>
2018-04-07 17:07:25 +0200

    db

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {# 'django.db.backends.mysql'
+# DATABASES = {# 'django.db.backends.mysql'
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DB_HOST', "localhost"),
+        'NAME': 'my_db',
+        'USER': 'my_db_user1',
+        'PASSWORD': 'My_db_pass1',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
